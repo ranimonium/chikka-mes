966d3b5c9369cbad32f19d49397df2756fd62b4b
Chikka <chikka@chikka-HP-Compaq-8000-Elite-SFF-PC.(none)>
2014-04-14 06:39:11 -0400

    broadcast

--- client.py
+++ client.py
@@ -1,6 +1,9 @@
 # client.py
 
 from socket import *
+import threading
+
+BUFFERSIZE = 2048
 
 # serverName = "127.0.0.1"
 serverName = "10.11.3.26"
@@ -9,9 +12,20 @@ serverPort = 1234
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
-msg = raw_input('Enter message: ')
 
-clientSocket.send(msg)
+def recv_msg():
+	while True:
+		msg = clientSocket.recv(BUFFERSIZE)
+		print msg
+
+st = threading.Thread(target=recv_msg)
+st.start()
+
+while True:
+	msg = raw_input()
+	clientSocket.send(msg)
+	if msg == "QUIT":
+		break
 
 clientSocket.close()
 
--- server.py
+++ server.py
@@ -1,56 +1,53 @@
 #server.py
 
 from socket import *
+import threading
 
 BUFFERSIZE = 2048
 
 serverPort = 1234
+
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
+
+
+#become a server socket
+serverSocket.listen(5)
+
 print "Server ready..."
 
-connectionSocket, addr = serverSocket.accept()
-msg = connectionSocket.recv(BUFFERSIZE)
-print addr, ": ", msg
+
+#connection socket list
+connectSocket_list = []
+
+def broadcast(msg, clientSocket):
+	for c in connectSocket_list:
+		if msg == "QUIT":
+			msg = "Client ", connectSocket_list.index(clientSocket), " has disconnected"
+		c.send(msg)
+
+def recv_msg(clientSocket):
+	while True:
+		msg = connectionSocket.recv(BUFFERSIZE)	
+		msg = "Client " + str(connectSocket_list.index(clientSocket)) + ": " + msg
+		print msg
+		broadcast(msg, clientSocket)
+		
+		#end thread, close connection
+		if msg == "QUIT":
+			clientSocket.close()
+			break
+
+
+while True:
+	connectionSocket, addr = serverSocket.accept()
+	connectionSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+	connectSocket_list.append(connectionSocket)
+	
+	ct = threading.Thread(target=recv_msg, args=(connectionSocket, ))
+	ct.start()
+	
+
 connectionSocket.close()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-
-serverSocket = socket(AF_INET, SOCK_DGRAM) #instantiate socket
-serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) #so we could reuse socket
-# serverSocket.bind(('', serverPort))
-
-
-print "Server ready..."
-
-while 1:
-	message, clientAddress = serverSocket.recvfrom(BUFFERSIZE)
-	print clientAddress, ": ", message
-
-"""
