be054cdc896aff6e129d3b8010e37b259e2c3c28
Chikka <chikka@chikka-HP-Compaq-8000-Elite-SFF-PC.(none)>
2014-04-15 03:26:52 -0400

    server can respond to curl; does not close connection though

--- server.py
+++ server.py
@@ -2,9 +2,10 @@
 
 from socket import *
 import threading
+import traceback
+
 
 BUFFERSIZE = 2048
-
 serverPort = 1234
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -12,7 +13,7 @@ serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 serverSocket.bind(('', serverPort))
 
 #become a server socket
-serverSocket.listen(3)
+serverSocket.listen(1)
 
 print "Server ready..."
 
@@ -27,25 +28,38 @@ def broadcast(msg):
 	except:
 		pass
 
+def respond(msg, connectionSocket):
+	msg = msg.split('\n')
+	method = msg[0].split(' ')[0]
+	httpversion = msg[0].split(' ')[2]
+	
+	response = ""
 
+	if method == "GET":
+		response = "HTTP/1.1 200 OK\r\n"
+		response += "Connection: close\r\n\r\n"
+		print response
+	elif method == "POST":
+		pass
+	elif method == "PUT":
+		pass
+	connectionSocket.send(response)
+	
 def recv_msg(connectionSocket):
 	try:
 		while True:
-			msg = connectionSocket.recv(BUFFERSIZE)	
-			if "QUIT" != msg:
-				msg = "Client " + str(connectSocket_list.index(connectionSocket)) + ": " + msg
-			else:
-				msg = "Client " + str(connectSocket_list.index(connectionSocket)) + " has disconnected."
-				connectSocket_list.remove(connectionSocket)
-			print msg
-			broadcast(msg)
+			msg = connectionSocket.recv(BUFFERSIZE)
+			print "MSG: " + msg
+			respond(msg, connectionSocket)
 			
 			#end thread, close connection
 			if msg == "QUIT":
 				connectionSocket.close()
 				sys.exit()
-	except:
-		pass
+	except Exception as e:
+		traceback.print_exc()
+		print e
+
 
 while True:
 	connectionSocket, addr = serverSocket.accept()
