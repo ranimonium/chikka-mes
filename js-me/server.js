d13af7d5c716c0c8532643ddb821ddd8a00dc547
Jadurani Davalos <jadurani.davalos@gmail.com>
2014-04-22 16:33:58 +0800

    DONE: basic webserver using NodeJS; with connect module from npm

--- js-me/server.js
+++ js-me/server.js
@@ -1,12 +1,2 @@
-var http = require("http");
-
-http.createServer(function(request, response) {
-	
-	response.writeHead(200, {"Content-Type": "text/sagf"});
-	response.write("Hello World\n");
-	// console.log(request['url'])
-	response.end();
-}).listen(8888);
-
-console.log("Server has started.");
-
+var connect = require('connect');
+connect().use(connect.static(__dirname)).listen(8888);
