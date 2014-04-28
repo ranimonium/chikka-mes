978c2530c71ff2132c353a7b0dfc0ab75d78194a
Jadurani Davalos <jadurani.davalos@gmail.com>
2014-04-28 14:04:05 +0800

    DONE: todolist similar to groupchat Server: NodeJS DB: redis mehehe: AJAX

--- js-me/server.js
+++ js-me/server.js
@@ -1,7 +1,6 @@
 var http = require('http');
-var fs = require('fs');
 var url = require('url');
-var qs = require('querystring');
+var fs = require('fs');
 var redis = require('redis'),
  redis_client = redis.createClient();
 
@@ -11,41 +10,57 @@ redis_client.on("error", function (err) {
 });
 
 var server = http.createServer(function(req, res){
+	var path = url.parse(req.url).pathname;
+	// console.log(req.url);
+	
+	if(path.search("add") != -1){
+		query = url.parse(req.url, true).query;
+		// console.log(query);
 
-	if(req.method == "POST") {
-		var reqBody = '';
+		if(query.task != undefined && query.task.length > 0){
+			redis_client.rpush('todolist', query.task);
+		}
 
-		req.on('data', function(data){
-			reqBody += data;
-			var formData = qs.parse(reqBody);
-			
-			if(formData.task != undefined && formData.task.length > 0){
-				redis_client.rpush('todolist', formData.task);
-			}
-			if(formData.removeLast != undefined && formData.removeLast){
-				redis_client.rpop('todolist');
-			}
-			updateToDoList(res);
-		});
+		//updatetodo
+		getToDoList(res);
+	} else if(path.search("remove")!= -1){
+		redis_client.rpop('todolist');		
+
+		//updatetodo
+		getToDoList(res);
+	}	else if(path.search("refresh") != -1){
+		getToDoList(res);
 	} else {
-			updateToDoList(res);
+		fs.readFile('./todo.html', function(err, file){
+			if(err){
+				console.log("Error: " + err);
+				return;
+			}
+			
+			//updatetodo
+			getToDoList(res, file+"");
+		});
 	}
-
 }).listen(8888);
 
 console.log('Server ready.');
 
-function updateToDoList(res){
-	var todoHTMLFile = fs.readFileSync('./todo.html', 'utf8');
+function getToDoList(res, file){
+
 	var todolist_result = '';
 	redis_client.lrange('todolist', 0, -1, function(err, result) {
+
 		for ( todoitem in result ) {
 			todolist_result += "<li>" + result[todoitem] + "</li>";
 		}
 
-		var todolistendindex = todoHTMLFile.search('<ul id="todolist">') + '<ul id="todolist">'.length;
-		todoHTMLFile = (todoHTMLFile.substring(0, todolistendindex) + todolist_result + todoHTMLFile.substring(todolistendindex));
-		res.writeHead(200, {"Content-Type": "text/html"});
-		res.end(todoHTMLFile);
+		if(file){
+			var insert_i = file.search('<ul id="todolist">') + '<ul id="todolist">'.length;
+			todolist_result = (file.substring(0, insert_i) + todolist_result + file.substring(insert_i));
+		}
+				
+		// console.log(todolist_result)
+		res.writeHead(200, {'Content-Type':'text/html'});
+		res.end(todolist_result, 'utf-8');
 	});
 }
